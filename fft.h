e1561fe70f277d6e67c45e21d22214181e8aa42f
V1nc3ntL <vincentlefebvre@gmx.com>
2020-05-30 17:09:04 +0200

    Reorganisation

--- fft.h
+++
@@ -1,6 +0,0 @@
-#ifndef __FFT_H__
-#define __FFT_H__
-void fftf_rdx2 (short *sig, float *TF, int N, float *twiddles);
-void fftf_rdx4 (short *sig, float *TF, int N, float *twiddles);
-float * get_twiddles (int b);
-#endif
